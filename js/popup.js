d12e4a2b41b7687aa6254277d903efb735a25eef
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-17 15:59:03 +0200

    feat: display all style colors

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,27 @@
 fontsDiv = document.querySelector('.fonts');
+colorsDiv = document.querySelector('.colors');
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {method: "getContent", data: "get data"}, function(response) {
-        dataReceived(response.data);
+        displayFonts(response.dataFonts);
+        displayColors(response.dataColors);
     });
   });
 
 
-function dataReceived(fonts) {
-    console.log(fonts);
+function displayFonts(fonts) {
      for (var i = 0; i < fonts.length; i++) { 
         listItem = document.createElement('li');
         listItem.innerText = fonts[i];
         fontsDiv.appendChild(listItem);
     }
+}
+
+function displayColors(colors) {
+    for (var i = 0; i < colors.length; i++) { 
+       listItem = document.createElement('li');
+       listItem.innerText = colors[i];
+       listItem.style.setProperty('--color', colors[i]);
+       colorsDiv.appendChild(listItem);
+   }
 }
