918941bd1fb40aaf8b7df2aa06954a59de0ec492
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-17 15:59:13 +0200

    feat: get all style colors

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ chrome.runtime.onMessage.addListener(
                   "from a content script:" + sender.tab.url :
                   "from the extension");
       if (request.method == "getContent") {
-        const data = styleInPage('fontFamily');
-        sendResponse({data: data});
+        const dataFonts = styleInPage('fontFamily');
+        const dataColors = getAllColors();
+        sendResponse({dataFonts: dataFonts, dataColors: dataColors});
       }
     }
   );
@@ -73,4 +74,41 @@ function styleInPage(css, verbose) {
         }
     }
     return allStyles
+}
+
+function getAllColors() {
+    // regex via http://stackoverflow.com/a/7543829/149636
+    var rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
+
+    var allColors = [];
+
+    var elems = document.getElementsByTagName('*');
+    var total = elems.length;
+
+    var x,y,elemStyles,styleName,styleValue,rgbVal;
+
+    for(x = 0; x < total; x++) {
+        elemStyles = window.getComputedStyle(elems[x]);
+
+        for(y = 0; y < elemStyles.length; y++) {
+            styleName = elemStyles[y];
+            styleValue = elemStyles[styleName];
+
+            if(!styleValue) {
+                continue;
+            }
+            // convert to string to avoid match exceptions
+            styleValue += "";
+
+            rgbVal = styleValue.match(rgbRegex);
+            if(!rgbVal) { // property does not contain a color
+                continue;
+            }
+
+            if(allColors.indexOf(rgbVal.input) == -1) { // avoid duplicate entries
+                allColors.push(rgbVal.input);
+            }
+        }
+    }
+    return allColors;
 }
